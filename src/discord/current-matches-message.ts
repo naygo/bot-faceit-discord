32e1539ea61f765d56abf69fb8988f082773cbbf
Nayla Gomes <nayla.cgs@gmail.com>
2023-03-31 17:45:06 -0300

    adicionando mais informacoes sobre as partidas em andamento

--- src/discord/current-matches-message.ts
+++ src/discord/current-matches-message.ts
@@ -1,10 +1,12 @@
 import { getHubMatches } from '@/faceit/faceit-api';
-import { FaceitHubMatches, Roster } from '@/models/interfaces/hub-matches';
+import { FaceitHubMatches, HubMatch, Roster } from '@/models/interfaces/hub-matches';
 import { getMessagesFaceitInfoChannel } from '@/utils/faceit-info-channel';
+import { sleep } from '@/utils/time';
 import { Client } from 'discord.js';
 
 interface CurrentMatchesInfo {
   matchName: string;
+  startedAt: string;
   playersName: {
     faction1: string[];
     faction2: string[];
@@ -13,41 +15,47 @@ interface CurrentMatchesInfo {
 
 export async function messageToShowCurrentMatches(client: Client) {
   const { messages, channel } = await getMessagesFaceitInfoChannel(client);
-  const currentMatches = await getHubMatches();
+  const matches = await getHubMatches();
+  const currentMatches = matches.items.filter(match => match.status !== 'CHECK_IN');
+  const checkInMatches = matches.items.filter(match => match.status === 'CHECK_IN');
 
   const message = messages.find((message) =>
     message.content.includes('PARTIDAS ATIVAS')
   );
 
-  const messageText = makeMessageText(mapCurrentMatchesInfo(currentMatches));
+  const messageText = makeCurrentMatchesMessageText(mapCurrentMatchesInfo(currentMatches));
 
   if (message) {
-    await message.edit(messageText);
+    await message.edit(`**[${checkInMatches.length}] PARTIDAS EM CHECK-IN**\n` + messageText);
   } else {
     await channel.send(messageText);
   }
+
+  sleep(5);
+  messageToShowCurrentMatches(client);
 }
 
-function makeMessageText(currentMatchesInfo: CurrentMatchesInfo[]): string {
+function makeCurrentMatchesMessageText(currentMatchesInfo: CurrentMatchesInfo[]): string {
   const messageText = currentMatchesInfo.reduce((acc, currentMatchInfo) => {
-    const { matchName, playersName } = currentMatchInfo;
-
+    const { matchName, playersName, startedAt } = currentMatchInfo;
+    
     const playersNameFaction1 = playersName.faction1.join('\n');
     const playersNameFaction2 = playersName.faction2.join('\n');
 
-    return `${acc} **\n----------- ${matchName} -----------**\n\n**PLAYERS TIME 1**\n${playersNameFaction1}\n\n**PLAYERS TIME 2**\n${playersNameFaction2}\n`;
+    return `${acc} **\n----------- ${matchName} ----------- ${startedAt}**\n\n**PLAYERS TIME 1**\n${playersNameFaction1}\n\n**PLAYERS TIME 2**\n${playersNameFaction2}\n`;
   }, `**[${currentMatchesInfo.length}] PARTIDAS ATIVAS**\n`);
 
   return messageText;
 }
 
 function mapCurrentMatchesInfo(
-  matches: FaceitHubMatches
+  matches: HubMatch[]
 ): CurrentMatchesInfo[] {
   const currentMatches: CurrentMatchesInfo[] = [];
-  for (const match of matches.items) {
+  for (const match of matches) {
     currentMatches.push({
       matchName: `${match.teams.faction1.name} **VS** ${match.teams.faction2.name}`,
+      startedAt: convertEpoch(match.started_at),
       playersName: {
         faction1: mapPlayerName(match.teams.faction1.roster),
         faction2: mapPlayerName(match.teams.faction2.roster),
@@ -62,4 +70,9 @@ function mapPlayerName(players: Roster[]) {
   return players.map(
     (player) => `${player.nickname} (${player.game_player_name})`
   );
+}
+
+function convertEpoch(epoch: number): string {
+  const date = new Date(epoch * 1000);
+  return date.toLocaleString().replace(',', '');
 }
