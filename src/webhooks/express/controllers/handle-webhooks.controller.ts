7f4ce49155a9c0bf7f9411083487922bbe134518
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-01 15:01:01 -0300

    feat: add log

--- src/webhooks/express/controllers/handle-webhooks.controller.ts
+++ src/webhooks/express/controllers/handle-webhooks.controller.ts
@@ -4,6 +4,9 @@ export async function handleWebhooksController(req: Request, res: Response) {
   try {
     const body = req.body;
 
+    console.log('handleWebhooksController ------------------------------------------ ')
+    console.log(body);
+
     return res.status(200).json({
       body
     });
